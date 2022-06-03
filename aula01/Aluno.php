b6d27ebcbd7cc9a280deb30300eab4c5278a5010
MBCapello <mbcapello@gmail.com>
2022-06-02 23:07:23 -0300

    aula 01

---
+++ aula01/Aluno.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+class Aluno //Clase
+{
+    public $nome; //Atributos
+    public $cpf; //Atributos
+}
---
+++ aula01/intro.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'Aluno.php';
+
+$a1 = new Aluno;
+$a1 -> nome = 'Alessandro';
+$a1 -> cpf = '111.222.333-45';
+
+$aluno1 = [
+    'nome' => 'Max',
+    'cpf' => '098.876.765-21',
+
+];
+$aluno2 = [
+    'nome' => 'Edu',
+    'CPF' => '011.823.995-29',
+];
+
+$aluno3 = [
+    'Nome' => 'Ze',
+    'cpf' => '498.842.125-31',
+];
